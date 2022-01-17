a06416d8b716e89dbacd003d73cb988b5615d504
noyan <63000297+no-yan@users.noreply.github.com>
2022-01-17 21:19:37 +0900

    wip: no-shorthand

--- src/rules/prefer-no-shorthand.ts
+++ src/rules/prefer-no-shorthand.ts
@@ -1,71 +1,49 @@
 import { Rule } from "eslint";
-import { Identifier } from "estree";
-import type { JSXOpeningElement, JSXIdentifier } from "estree-jsx";
-
-const chakraComponentGroup = [
-  ["Box", true],
-  ["Flex", true],
-  ["Button", true],
-] as const;
-const chakraMap = new Map<string, boolean>(chakraComponentGroup);
-
-// 検討：JSXIdentifierに統一
-const isChakraComponent = (node: Identifier | JSXIdentifier) => {
-  const componentName = node.name;
-  const doesExist = chakraMap.get(componentName);
-  if (doesExist) {
-    return true;
-  }
-  return false;
-};
+import { Identifier, ImportDeclaration } from "estree";
+import type { JSXIdentifier } from "estree-jsx";
+import { isChakraComponent } from "../utils/isChakraComponent";
 
 const rule: Rule.RuleModule = {
   meta: {
     fixable: "code",
   },
   create: context => {
+    const importedMap = new Map<string, true>();
+
     return {
-      //   "JSXAttribute > JSXIdentifier[name='height'] > ": (node: Identifier) => {
-      //     if (isChakraComponent(node)) {
-      //       context.report({ message: "Don't use 'non-chakra Component'", node });
-      //     }
-      //   },
-      "JSXAttribute > JSXIdentifier[name='h']": (targetAttribute: Identifier) => {
-        // `context.getAncestors` returns an array of the ancestors of the currently-traversed
-        // node, starting at the root of the AST and continuing through the direct parent
-        // of the current node. This array does not include the currently-traversed node itself.
+      ImportDeclaration: (node: ImportDeclaration) => {
+        updateImportedMap(importedMap, node);
+      },
+      "JSXAttribute > JSXIdentifier": (targetAttribute: Identifier) => {
+        const property = targetAttribute.name;
 
-        // We want to know if the component name belongs to Chakra UI, so we look at the parent element.
-        // This is the second-to-last element of the array.
-        const ancestorsList = context.getAncestors();
-        const JSXOpeningElement = ancestorsList[ancestorsList.length - 2] as unknown as JSXOpeningElement;
-        const maybeJSXIdentifier = JSXOpeningElement.name;
-        if (!(maybeJSXIdentifier.type === "JSXIdentifier")) {
+        if (!isChakraComponent(targetAttribute, importedMap)) {
           return;
-        }
-        // It is identifier.
-        // Because Estree doesn't know JSX syntax, we'll convevrt type from JSXIdentifer from Identifier.
-        const identifier = maybeJSXIdentifier as unknown as Identifier;
-        if (!isChakraComponent(identifier)) {
-          return;
+          // const fix = (fixer: Rule.RuleFixer) => {
+          //   if (!targetAttribute.range) {
+          //     // guard for undefined
+          //     return null;
+          //   }
+          //   return fixer.replaceTextRange(targetAttribute.range, "height");
+          // };
         }
 
-        const fix = (fixer: Rule.RuleFixer) => {
-          if (!targetAttribute.range) {
-            // guard for undefined
-            return null;
-          }
-          return fixer.replaceTextRange(targetAttribute.range, "height");
-        };
+        // TODO: chakraのプロパティに`property`があるか確認する。
 
         context.report({
           message: "Don't use 'h', use instead 'height'",
           node: targetAttribute,
-          fix,
+          // fix,
         });
+      },
+      "Program:exit": function reportAndReset() {
+        importedMap.clear();
       },
     };
   },
 };
 
 export = rule;
+function updateImportedMap(importedMap: Map<string, true>, importedList: any) {
+  throw new Error("Function not implemented.");
+}
---
+++ src/utils/shorthandsTable.ts
@@ -0,0 +1,45 @@
+export const shorthands = [
+  //https://chakra-ui.com/docs/features/style-props#margin-and-padding
+  ["m", "margin"],
+  ["mt", "marginTop"],
+  ["mr", "marginRight"],
+  ["me", "marginEnd"],
+  ["mb", "marginBottom"],
+  ["ml", "marginLeft"],
+  ["ms", "marginStart"],
+  ["p", "padding"],
+  ["pt", "paddingTop"],
+  ["pr", "paddingRight"],
+  ["pe", "paddingEnd"],
+  ["pb", "paddingBottom"],
+  ["pl", "paddingLeft"],
+  ["ps", "paddingStart"],
+
+  // https://chakra-ui.com/docs/features/style-props#color-and-background-color
+  ["bg", "background"],
+
+  // https://chakra-ui.com/docs/features/style-props#gradient
+  ["bgClip", "backgroundClip"],
+
+  // https://chakra-ui.com/docs/features/style-props#layout-width-and-height
+  ["w", "width"],
+  ["h", "height"],
+  ["minW", "minWidth"],
+  ["maxW", "maxWidth"],
+  ["minH", "minHeight"],
+  ["maxH", "maxHeight"],
+  ["d", "display"],
+
+  // https://chakra-ui.com/docs/features/style-props#flexbox
+
+  // https://chakra-ui.com/docs/features/style-props#grid-layout
+  ["bg", "background"],
+  ["bgImage", "backgroundImage"],
+  ["bgSize", "backgroundSize"],
+  ["bgPosition", "backgroundPosition"],
+  ["bgRepeat", "backgroundRepeat"],
+  ["bgAttachment", "backgroundAttachment"],
+
+  // https://chakra-ui.com/docs/features/style-props#position
+  ["pos", "position"],
+] as const;
