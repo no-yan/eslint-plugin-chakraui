db0d8c343c5ff3f84f602ae86c983b3f4b3af463
noyan <63000297+no-yan@users.noreply.github.com>
2022-01-20 19:03:47 +0900

    delete comment out

--- src/rules/props-order.ts
+++ src/rules/props-order.ts
@@ -45,73 +45,14 @@ const sortProperties = (properties: JSXAttribute[]) => {
 
 const getPropertyNameAndValue = (node: JSXAttribute, sourceCode: SourceCode) => {
   const propName = node.name.name;
-
+  // This gets full text, like {1}, {"string"}, {Variable}, "string".
+  // By keeping it enclosed in brackets or quotes, the logic of replace will be easier.
   const propValue = sourceCode.getText(node.value as unknown as Node);
-  // switch (true) {
-  //   case node?.value?.expression?.value !== undefined:
-  //     // string case. e.g. <X attr='hoge'/>
-  //     propValue = node?.value?.expression?.value;
-  //     break;
-  //   case node?.value?.expression?.name !== undefined:
-  //     // variableCase. e.g. <X attr={Variable}
-  //     propValue = node.value.expression.value;
-  //     break;
-  //   default:
-  //     // <X isBoolean/> case
-  //     propValue = undefined;
-  // }
-  // console.log(propName, propValue);
+
   return {
     propName,
     propValue,
   };
-};
-
-// const getPropertyNameAndValueFromSource = (node: JSXAttribute, sourceCode: SourceCode) => {
-//   const propName = node.name.name;
-
-//   const propValue = sourceCode.getText(node.value as unknown as Node);
-//   // switch (true) {
-//   //   case node?.value?.expression?.value !== undefined:
-//   //     // string case. e.g. <X attr='hoge'/>
-//   //     propValue = node?.value?.expression?.value;
-//   //     break;
-//   //   case node?.value?.expression?.name !== undefined:
-//   //     // variableCase. e.g. <X attr={Variable}
-//   //     propValue = node.value.expression.value;
-//   //     break;
-//   //   default:
-//   //     // <X isBoolean/> case
-//   //     propValue = undefined;
-//   // }
-//   console.log(propValue);
-//   return {
-//     propName,
-//     propValue,
-//   };
-// };
-
-const concatSortedText = (sorted: JSXAttribute[], unsorted: JSXAttribute[], sourceCode: SourceCode) => {
-  let sortedText = "";
-
-  for (let i = 0; i < sorted.length; i++) {
-    const sortedAttr = sorted[i] as unknown as Node;
-
-    sortedText += sourceCode.getText(sortedAttr);
-
-    // add space or break line
-    if (i !== sorted.length - 1) {
-      const isNewLine = unsorted[i].loc?.end.line !== unsorted[i + 1].loc?.start.line;
-
-      if (isNewLine) {
-        // this doesn't keep its spacing as it was.
-        sortedText += "\n";
-      } else {
-        sortedText += " ";
-      }
-    }
-  }
-  return sortedText;
 };
 
 const rule: Rule.RuleModule = {
@@ -167,26 +108,6 @@ const rule: Rule.RuleModule = {
                 continue;
               }
 
-              // 問題：Fixでプロパティの値を変更するときに、２つの値の比較が不正だと言われる。
-              // why?
-              // WORKAROUND? :   replaceTextRange()で変更
-              // replaceTextRange(node.value.range,nextValue)
-
-              // let targetNode;
-              // switch (true) {
-              //   case node?.value?.expression?.value !== undefined:
-              //     // string case. e.g. <X attr='hoge'/>
-              //     targetNode = node?.value?.expression?.value;
-              //     break;
-              //   case node?.value?.expression?.name !== undefined:
-              //     // variableCase. e.g. <X attr={Variable}
-              //     targetNode = node.value.expression.value;
-              //     break;
-              //   default:
-              //     // <X isBoolean/> case
-              //     targetNode = undefined;
-              // }
-              // console.log(targetNode);
               const range = node?.value?.range;
               if (!range) {
                 continue;
@@ -195,27 +116,9 @@ const rule: Rule.RuleModule = {
               fixingArray.push(fixValue);
               fixingArray.push(fixName);
             }
-
-            // if (node.name.name !== sortedNode.name.name) {
-            //   console.log(i);
-            //   console.log(node.name.name);
-            //   // @ts-ignore
-            //   console.log(node?.value?.expression?.value);
-            //   if (node.value.expression.value === undefined) {
-            //     const isVariable = true;
-            //     const name = node?.value?.expression?.name;
-            //     console.log(name);
-            //     // if (name === null) {
-            //     //   console.log(node);
-            //     // }
-            //     // node.nameはあるが、node.value:null のケース e.g. <Div isHover>
-            //     // replaceTextでnode全体を入れ替え？
-            //   }
-            // }
           }
           return fixingArray;
         };
-        // let sortedText = concatSortedText(sorted, unsorted, sourceCode);
 
         if (shouldFix) {
           const nodeStart = unsorted[0].range?.[0];
